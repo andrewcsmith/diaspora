3370388fa3fd30cba7588c315e3b5bc7567fed0e
Andrew Smith <andrewchristophersmith@gmail.com>
2013-07-21 15:57:41 -0700

    Added very very basic Wordpress cross-posting functionality.

--- app/models/services/wordpress.rb
+++ app/models/services/wordpress.rb
@@ -1,23 +1,48 @@
-require 'xmlrpc/client'
-
 class Services::Wordpress < Service
   include ActionView::Helpers::TextHelper
   include MarkdownifyHelper
   
+  MAX_CHARACTERS = 1000
+  
   attr_accessor :username, :password, :host, :path
+  
+  # uid = blog_id
   
   def provider
     "wordpress"
   end
   
-  def post(post, url='')
-    s = XMLRPC::Client.new(@host, @path, 80)
-    wp_post_options = {:post_content => post.text, :post_status => 'publish'}
-    res = s.call('wp.newPost', 0, username, password, wp_post_options)
-    res
+  def consumer_client_id
+    AppConfig.services.wordpress.client_id
   end
   
-  def url
-    "#{host}:80#{path}"
+  def consumer_secret
+    AppConfig.services.wordpress.secret
   end
+  
+  def post(post, url='')
+    access_key = self.access_token
+    
+    conn = Faraday.new(:url => "https://public-api.wordpress.com")
+    res = conn.post do |req|
+      req.url "/rest/v1/sites/#{self.uid}/posts/new"
+      req.body = post_body(post)
+      req.headers['Authorization'] = "Bearer #{access_key}"
+      req.headers['Content-Type'] = 'application/json'
+    end
+    res_body = JSON.parse res.env[:body]
+    res_body
+  end
+  
+  def post_body(post, url='')
+    post_text = strip_markdown(post.text(:plain_text => true))
+    post_title = truncate(post_text, :length => 40, :separator => ' ')
+    
+    body = {
+      :title => post_title,
+      :content => post_text
+    }
+    body.to_json
+  end
+  
 end
--- spec/models/services/wordpress_spec.rb
+++ spec/models/services/wordpress_spec.rb
@@ -1,19 +1,23 @@
 require 'spec_helper'
-require 'xmlrpc/client'
 
 describe Services::Wordpress do
   
   before do
     @user = alice
-    @post = @user.post(:status_message, :text => "hello", :to => @user.aspects.first.id)
-    @service = Services::Wordpress.new(:username => "admin", :password => "password", :host => "www.myblog.com", :path => "/wordpress/xmlrpc.php")
+    @post = @user.post(:status_message, 
+                       :text => "hello", 
+                       :to => @user.aspects.first.id)
+                       
+    @service = Services::Wordpress.new(:nickname => "andrew", 
+                                       :access_token => "abc123", 
+                                       :uid => "123")
     @user.services << @service
   end
   
   describe "#post" do
     
     it 'posts a status message to wordpress' do
-      stub_request(:post, @service.url).to_return(:status => 200, :body => XMLRPC::Create.new.methodResponse(true, {:post_id => "42"}))
+      stub_request(:post, "https://public-api.wordpress.com/rest/v1/sites/123/posts/new").with(:body => {:title => "hello", :content => "hello"}.to_json).to_return(:status => 200, :body => {:ID => 68}.to_json, :headers => {})
       @service.post(@post)
     end
     
